0e15478840f9a79c3f0454845fbf4e191aa6973b
Hasitha Wikramasinghe <hasitha@corzent.com>
2024-04-16 08:05:47 +0530

    UI implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,71 @@
-function App() {
+import Home from "./pages/home/Home"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom"
+import Users from "./pages/users/Users"
+import Products from "./pages/products/Products"
+import Login from "./pages/login/login"
+import Navbar from "./components/navbar/navbar"
+import Menu from "./components/menu/menu"
+import Footer from "./components/footer/footer"
+import "./styles/global.scss"
+import User from "./pages/user/user"
+import Product from "./pages/product/Product"
 
-  return (
-    <div>Hello World</div>
-  )
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "/users",
+          element: <Users/>
+        },
+        {
+          path: "/products",
+          element: <Products />
+        },
+        {
+          path: "/users/:id",
+          element: <User />
+        },
+        {
+          path: "/products/:id",
+          element: <Product />
+        }
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />
+    }
+  ])
+
+  return <RouterProvider router={router}/>
 }
 
 export default App
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span>Lamaadmin</span>
+            <span>© Lama dev admin dashboard</span>
+        </div>
+    )
+}
+
+export default Footer;
